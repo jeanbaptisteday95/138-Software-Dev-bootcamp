c43fb79f7e719d10d3d5487fe95d6f844f601379
Gurneesh Singh <gurneesh@Gurneeshs-MacBook-Air.local>
2026-07-23 21:18:24 -0400

    add content for DSA lesson 3

--- Unit6- DS & Algo/Lesson2 - Linked Lists & Recusion/practice.js
+++ Unit6- DS & Algo/Lesson2 - Linked Lists & Recusion/practice.js
@@ -1,38 +1,14 @@
 // Question 1: Flatten nested array
 
 function flattenArray(arr) {
-  let result = [];
 
-  for (let element of arr) {
-    // If element is an array, recursively flatten it
-    if (Array.isArray(element)) {
-      result = result.concat(flattenArray(element));
-    } else {
-      // If element is not an array, add it directly
-      result.push(element);
-    }
-  }
-
-  return result;
 };
 
-console.log(flattenArray([1, [2, [3, 4], 5], 6]));
-
+//console.log(flattenArray([1, [2, [3, 4], 5], 6]));
 // Question 2: Check Palindrome
 
 function isPalindrome(str) {
-  // Base case: if string is 0 or 1 character, it's a palindrome
-  if (str.length <= 1) {
-    return true;
-  }
 
-  // Compare first and last characters
-  if (str[0] !== str[str.length - 1]) {
-    return false;
-  }
-
-  // Recursively check the substring without the first and last characters
-  return isPalindrome(str.slice(1, -1));
 }
-console.log(isPalindrome("level")); // true
-console.log(isPalindrome("hello")); // false
+// console.log(isPalindrome("level")); // true
+// console.log(isPalindrome("hello")); // false
